e88bc0ccd853144f8a663a6f9e1cbc4bac9fdd7e
bfrasure <bill.frasure@gmail.com>
2019-03-20 15:01:17 -0600

    Tweak to verify lambda is being called

--- lambdas/hello.go
+++ lambdas/hello.go
@@ -3,12 +3,15 @@ package main
 import (
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+  "fmt"
 )
 
 func handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+	const name, age = "Kim", 22
+	fmt.Println(name, "is", age, "years old.")
 	return &events.APIGatewayProxyResponse{
 		StatusCode: 200,
-		Body:       "Hello, World",
+		Body:       "Hello, World!!",
 	}, nil
 }
 

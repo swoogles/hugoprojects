1bf13186cb549595c21483dbf302ee44fe2f46b6
bfrasure <bill.frasure@gmail.com>
2019-03-20 16:40:05 -0600

    Cleanup go web-service function

--- lambdas/vanillaGo/access-web-service.go
+++ lambdas/vanillaGo/access-web-service.go
@@ -5,18 +5,22 @@ import (
 	// "github.com/aws/aws-lambda-go/lambda"
   	"encoding/json"
   	"fmt"
-  	"log"
   	"time"
   	"net/http"
     // "net/url"
 )
 
-type CommitList struct {
-    Commits []Commit
+type SmallCommit struct {
+    Message string
 }
 
-type Commit struct {
+type CommitList struct {
+    Commits []GitHubCommit
+}
+
+type GitHubCommit struct {
     Sha string
+    Commit SmallCommit
 }
 
 var myClient = &http.Client{Timeout: 10 * time.Second}
@@ -41,67 +45,20 @@ func main() {
 
     	// url := fmt.Sprintf("/orgs/swoogles/repos", safePhone)
     	// url := fmt.Sprintf("https://api.github.com/repos/swoogles/Physics/commits")
-    	url := fmt.Sprintf("https://api.github.com/repos/swoogles/Physics/commits/8b415d488aec897b7d8fda662e0ef1b99b4c34a7")
 
-    	// Build the request
-    	req, err := http.NewRequest("GET", url, nil)
-    	if err != nil {
-            fmt.Println(err)
-    		log.Fatal("NewRequest: ", err)
-    		return
-    		}
-            fmt.Println(req)
-        	// For control over HTTP client headers,
-        	// redirect policy, and other settings,
-        	// create a Client
-        	// A Client is an HTTP client
         	client := &http.Client{}
 
-        	// Send the request via a client
-        	// Do sends an HTTP request and
-        	// returns an HTTP response
-        	resp, err := client.Do(req)
-        	if err != nil {
-        		log.Fatal("Do: ", err)
-        		return
-        	}
 
-        	// Callers should close resp.Body
-        	// when done reading from it
-        	// Defer the closing of the body
-            defer resp.Body.Close()
-        	fmt.Println(json.NewDecoder(resp.Body))
-        	// var record Numverify
-
-            foo1 := new(Commit) // or &Foo{}
-            getJson("https://api.github.com/repos/swoogles/Physics/commits/8b415d488aec897b7d8fda662e0ef1b99b4c34a7", foo1)
-            println(foo1.Sha)
-
-            var commitList []Commit
-            // commitList := [2] Commit{ Commit {}, Commit {} }// or &Foo{}
-            getJson("https://api.github.com/repos/swoogles/Physics/commits", commitList)
-            fmt.Println(commitList)
-
-
-        	// Use json.Decode for reading streams of JSON data
-        	// if err := json.NewDecoder(resp.Body).Decode(&record); err != nil {
-        		// log.Println(err)
-        // }
-    // fmt.Println(record)
-        req2, err := http.NewRequest("GET", "https://api.github.com/repos/swoogles/Physics/commits", nil)
+        req2, _ := http.NewRequest("GET", "https://api.github.com/repos/swoogles/Physics/commits", nil)
         req2.SetBasicAuth("swoogles", "84966102c7b7c430d40e6c0ae372916d83d51506")
 
-        // req2.Header.Add("Authorization","Basic " + basicAuth("swoogles","84966102c7b7c430d40e6c0ae372916d83d51506"))
-
-        resp2, err2 := client.Do(req2)
+        resp2, _ := client.Do(req2)
             defer resp2.Body.Close()
 
-
-            fmt.Println(commitList)
-            var commitList2 []Commit
+            var commitList2 []GitHubCommit
     json.NewDecoder(resp2.Body).Decode(&commitList2)
-            fmt.Println(resp2.Body)
-            fmt.Println(err2)
-            fmt.Println(commitList2)
+                for i := 0; i < len(commitList2); i++ {
+                    fmt.Println(commitList2[i].Sha + " " + commitList2[i].Commit.Message)
+                }
     fmt.Println("goodbye world")
 }

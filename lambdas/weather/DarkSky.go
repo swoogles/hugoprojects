ed82d338237360a6232c47a1f145d6fc124b72dd
bfrasure <bill.frasure@gmail.com>
2019-03-24 17:59:52 -0600

    +TimePeriodData to Forecast structs

--- lambdas/weather/DarkSky.go
+++ lambdas/weather/DarkSky.go
@@ -8,6 +8,11 @@ import (
 	"time"
 )
 
+type TimePeriodData struct {
+	Summary string // This isn't handling some symbols, like '<'
+	Icon string
+}
+
 type DataPoint struct {
 	ApparentTemperature float64
 	Temperature float64
@@ -15,11 +20,14 @@ type DataPoint struct {
 	CloudCover float64
 	PrecipIntensity float64
 	PrecipProbability float64
+	WindSpeed float64
 }
 
 type ForeCast struct {
 	Timezone string
 	Currently DataPoint
+	Hourly TimePeriodData
+	Daily TimePeriodData
 }
 
 var myClient = &http.Client{Timeout: 10 * time.Second}
@@ -27,8 +35,21 @@ var myClient = &http.Client{Timeout: 10 * time.Second}
 //func getForeCastJson(url string) ForeCast {
 //}
 
+func getTestData(url string) ForeCast {
+	var decodedForeCast ForeCast
+	error := json.Unmarshal(json.RawMessage(SampleData()), &decodedForeCast)
+	if error != nil {
+		panic(error)
+	}
+	return decodedForeCast
+}
+
 func GetBasicForecast(darkSkyToken string) ForeCast {
-	req3, _ := http.NewRequest("GET", "https://api.darksky.net/forecast/" + darkSkyToken + "/38.8697,-106.9878", nil)
+	req3, _ := http.NewRequest(
+		"GET",
+		"https://api.darksky.net/forecast/" +
+			darkSkyToken +
+			"/38.8697,-106.9878", nil)
 	resp3, error := myClient.Do(req3)
 	if (error != nil) {
 		fmt.Fprintf(os.Stderr, "error: %v\n", error)

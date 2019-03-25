c21557fffcadb54a4d691c1ec97fd57b77fc9a07
bfrasure <bill.frasure@gmail.com>
2019-03-24 22:23:08 -0600

    Possible implement live movie Data retrieval

--- lambdas/cbmajestic/Movies.go
+++ lambdas/cbmajestic/Movies.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"net/http"
+	"os"
+	"time"
 )
 
 type Rating struct {
@@ -43,10 +47,28 @@ return weatherForecast;
 	*/
 }
 
+var myClient = &http.Client{Timeout: 10 * time.Second}
+
 func handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+	var openMoviesDbToken = os.Getenv("DARK_SKY_TOKEN")
+
+	guardiansOfTheGalaxy2Url := "http://www.omdbapi.com/?i=tt3896198&apikey=" + openMoviesDbToken
+	movieRequest, _ := http.NewRequest(
+		"GET",
+		guardiansOfTheGalaxy2Url,
+		nil)
+	resp3, error := myClient.Do(movieRequest)
+	if (error != nil) {
+		fmt.Fprintf(os.Stderr, "error: %v\n", error)
+		os.Exit(1)
+	}
+
+	defer resp3.Body.Close()
+	var movie Movie
+	json.NewDecoder(resp3.Body).Decode(&movie)
 	return &events.APIGatewayProxyResponse{
 		StatusCode: 200,
-		Body:       "Movie content!",
+		Body:       movie.Title,
 	}, nil
 }
 

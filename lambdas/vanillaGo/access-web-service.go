c68d41ad2bdd14c1c33dbbbd55c6b6540d30d599
bfrasure <bill.frasure@gmail.com>
2019-05-18 17:07:06 -0600

    Re-enable old lambda function. Might be better off simply removed.

--- lambdas/vanillaGo/access-web-service.go
+++ lambdas/vanillaGo/access-web-service.go
@@ -3,68 +3,57 @@ package main
 import (
 	// "github.com/aws/aws-lambda-go/events"
 	// "github.com/aws/aws-lambda-go/lambda"
-  	"encoding/json"
-  	"time"
-  	"net/http"
-  	//"io/ioutil"
-    // "net/url"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+	//"io/ioutil"
+	// "net/url"
 )
 
 type SmallCommit struct {
-    Message string
+	Message string
 }
 
 type CommitList struct {
-    Commits []GitHubCommit
+	Commits []GitHubCommit
 }
 
 type GitHubCommit struct {
-    Sha string
-    Commit SmallCommit
+	Sha    string
+	Commit SmallCommit
 }
 
 type ForeCast struct {
-    Timezone string //`json:"timezone"`
+	Timezone string //`json:"timezone"`
 }
-
 
 var myClient = &http.Client{Timeout: 10 * time.Second}
 
 func getJson(url string, target interface{}) error {
-    r, err := myClient.Get(url)
-    if err != nil {
-        return err
-    }
-    defer r.Body.Close()
+	r, err := myClient.Get(url)
+	if err != nil {
+		return err
+	}
+	defer r.Body.Close()
 
-    return json.NewDecoder(r.Body).Decode(target)
+	return json.NewDecoder(r.Body).Decode(target)
 }
 
-/*
 func main() {
-    client := &http.Client{}
+	client := &http.Client{}
 
-    req2, _ := http.NewRequest("GET", "https://api.github.com/repos/swoogles/Physics/commits", nil)
-    req2.SetBasicAuth("swoogles", "e67b98d1a2d17730e45e9e26182f44d77985597b")
+	req2, _ := http.NewRequest("GET", "https://api.github.com/repos/swoogles/Physics/commits", nil)
+	req2.SetBasicAuth("swoogles", "e67b98d1a2d17730e45e9e26182f44d77985597b")
 
-    resp2, _ := client.Do(req2)
-    defer resp2.Body.Close()
+	resp2, _ := client.Do(req2)
+	defer resp2.Body.Close()
 
-    var finalText string = "This is live data: \n"
-            var commitList2 []GitHubCommit
-    json.NewDecoder(resp2.Body).Decode(&commitList2)
-                for i := 0; i < len(commitList2); i++ {
-                    finalText += commitList2[i].Sha + "\n    " + commitList2[i].Commit.Message + "\n\n"
-                }
-    fmt.Println(finalText)
-    /*
-    if resp3.StatusCode == http.StatusOK {
-        bodyBytes, _ := ioutil.ReadAll(resp3.Body)
-        bodyString := string(bodyBytes)
-        fmt.Println("BodyString: " + bodyString)
-    }
-    */
-    /*
+	var finalText string = "This is live data: \n"
+	var commitList2 []GitHubCommit
+	json.NewDecoder(resp2.Body).Decode(&commitList2)
+	for i := 0; i < len(commitList2); i++ {
+		finalText += commitList2[i].Sha + "\n    " + commitList2[i].Commit.Message + "\n\n"
+	}
+	fmt.Println(finalText)
 }
-
-*/

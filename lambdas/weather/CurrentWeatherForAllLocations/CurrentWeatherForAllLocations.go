e8e73a71d16b10086f94aef96ec0707784986040
bfrasure <bill.frasure@gmail.com>
2019-05-19 22:19:06 -0600

    Use proper ColoradoWeatherMap classes

--- lambdas/weather/CurrentWeatherForAllLocations/CurrentWeatherForAllLocations.go
+++ lambdas/weather/CurrentWeatherForAllLocations/CurrentWeatherForAllLocations.go
@@ -4,18 +4,19 @@ import (
 	"encoding/json"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/swoogles/ColoradoWeatherMap/darksky"
+
 	//"github.com/swoogles/hugoprojects/lambdas/weather"
-	"github.com/swoogles/hugoprojects/lambdas/weather"
 	"os"
 )
 
 func handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
 	var darkSkyToken = os.Getenv("DARK_SKY_TOKEN")
 
-	mountainCoordinates := []weather.GpsCoordinates{
+	mountainCoordinates := []darksky.GpsCoordinates{
 		{38.8697, -106.9878, "Crested Butte"},
 	}
-	var weatherForecast = weather.GetBasicForecast(darkSkyToken, mountainCoordinates[0], "Crested Butte")
+	var weatherForecast = darksky.GetBasicForecast(darkSkyToken, mountainCoordinates[0], "Crested Butte")
 	out, _ := json.MarshalIndent(weatherForecast, "", "  ")
 	weatherText := string(out)
 	return &events.APIGatewayProxyResponse{
